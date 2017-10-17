aa13efa8c468a65a51a50e9cb107a7f0130dca7c
Dan Pelensky <dan@8thlight.com>
2017-10-17 14:03:58 +0100

    moves winning scenarios into lines

--- src/lines.rs
+++ src/lines.rs
@@ -1,4 +1,5 @@
 use board::*;
+use board_formatter::expand_board;
 
 pub fn split_board_into_rows(expanded_board: &[String], size: i32) -> Vec<Vec<String>> {
     let chunks = expanded_board.chunks(size as usize);
@@ -36,11 +37,22 @@ pub fn find_right_diagonal(rows: &[Vec<String>]) -> Vec<String> {
     diagonal
 }
 
+pub fn find_all_lines(board: &Board) -> Vec<Vec<String>> {
+    let mut winning_scenarios: Vec<Vec<String>> = Vec::new();
+    let mut rows = split_board_into_rows(&expand_board(board), board.get_size().abs());
+    let mut columns = find_columns(&rows);
+    let left = find_left_diagonal(&rows);
+    let right = find_right_diagonal(&rows);
+    winning_scenarios.append(&mut rows);
+    winning_scenarios.append(&mut columns);
+    winning_scenarios.push(left);
+    winning_scenarios.push(right);
+    winning_scenarios
+}
+
 pub mod tests {
     #[cfg(test)]
     use super::*;
-    #[cfg(test)]
-    use board_formatter::expand_board;
     #[cfg(test)]
     use board::tests::set_up_board;
 
@@ -138,4 +150,21 @@ pub mod tests {
             let diagonal: Vec<String> = vec!["O".to_string(), "O".to_string(), "X".to_string()];
             assert_eq!(diagonal, find_right_diagonal(&rows));
     }
+
+    #[test]
+    fn finds_winning_scenarios() {
+        let board = set_up_board(3, vec![0, 4, 8, 2, 6, 7, 1, 3, 5]);
+        let winning_scenarios: Vec<Vec<String>> = vec![
+            vec!["X".to_string(), "X".to_string(), "O".to_string()],
+            vec!["O".to_string(), "O".to_string(), "X".to_string()],
+            vec!["X".to_string(), "O".to_string(), "X".to_string()],
+            vec!["X".to_string(), "O".to_string(), "X".to_string()],
+            vec!["X".to_string(), "O".to_string(), "O".to_string()],
+            vec!["O".to_string(), "X".to_string(), "X".to_string()],
+            vec!["X".to_string(), "O".to_string(), "X".to_string()],
+            vec!["O".to_string(), "O".to_string(), "X".to_string()],
+        ];
+            assert_eq!(winning_scenarios, find_all_lines(&board));
+    }
+
 }
