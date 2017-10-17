2a5d782b48ba3ba9ea3bc09bbc2d5936af4e98d2
Dan Pelensky <dan@8thlight.com>
2017-10-17 16:10:51 +0100

    Removes import all statements from app_runner

--- src/app_runner.rs
+++ src/app_runner.rs
@@ -1,8 +1,12 @@
-use io::*;
-use board::*;
-use game::*;
-use players::*;
-use game_types::*;
+use io;
+use board;
+use game;
+use players;
+use game_types;
+use io::display;
+use io::clear_screen;
+use board::Board;
+use players::Players;
 use board_printer::format_board;
 
 const INVALID_VALUE: i32 = -1;
@@ -12,21 +16,21 @@ const YES: i32 = 1;
 pub fn start() {
     let mut board = setup_board();
     let players = setup_players();
-    while !is_game_over(&board) {
+    while !game::is_game_over(&board) {
         board = single_turn(board, &players);
     }
     end_of_game(&board);
 }
 
 fn setup_board() -> Board {
-    clear_screen();
-    display(TITLE);
-    build_board(NUMBER_OF_ROWS)
+    io::clear_screen();
+    display(io::TITLE);
+    board::build_board(NUMBER_OF_ROWS)
 }
 
 fn setup_players() -> Vec<Players> {
-    let players = ask_player_type();
-    create_players(players)
+    let players = io::ask_player_type();
+    game_types::create_players(players)
 }
 
 fn single_turn(board: Board, players: &[Players]) -> Board {
@@ -37,26 +41,26 @@ fn single_turn(board: Board, players: &[Players]) -> Board {
 
 fn end_of_game(board: &Board) {
     clear_screen();
-    display(&alert_winner(&find_winner(board)));
+    display(&io::alert_winner(&game::find_winner(board)));
     display(&format_board(board));
-    if ask_play_again() == YES {
+    if io::ask_play_again() == YES {
         start()
     }
 }
 
 fn select_a_space(board: &Board, players: &[Players]) -> i32 {
-    let current_player_marker = find_current_player(board);
+    let current_player_marker = game::find_current_player(board);
     let mut space = INVALID_VALUE;
     players
         .iter()
-        .filter(|player| get_marker(player) == current_player_marker)
-        .for_each(|player| space = choose_space(player, board));
+        .filter(|player| players::get_marker(player) == current_player_marker)
+        .for_each(|player| space = players::choose_space(player, board));
     space
 }
 
 fn show_user_state_of_game(board: &Board) {
-    let current_player_marker = find_current_player(board);
+    let current_player_marker = game::find_current_player(board);
     clear_screen();
-    display(&select_space(&current_player_marker));
+    display(&io::select_space(&current_player_marker));
     display(&format_board(board));
 }
