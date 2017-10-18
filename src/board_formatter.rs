3f9577e3d0d43831a51ae31802f116e389937759
Dan Pelensky <dan@8thlight.com>
2017-10-18 13:48:58 +0100

    extracts loop

--- src/board_formatter.rs
+++ src/board_formatter.rs
@@ -7,14 +7,17 @@ pub fn expand_board(board: &Board) -> Vec<String> {
     let number_of_spaces = board.get_size() * board.get_size();
     let mut expanded_board: Vec<String> = vec![" ".to_string(); number_of_spaces as usize];
     for (index, space) in spaces.iter().enumerate() {
-        let marker = if is_even(index) {
-            marker::inspect(&Marker::X)
-        } else {
-            marker::inspect(&Marker::O)
-        };
-        expanded_board[*space as usize] = marker;
+        expanded_board[*space as usize] = marker::inspect(&set_marker(index));
     }
     expanded_board
+}
+
+fn set_marker(index: usize) -> Marker {
+        if is_even(index) {
+            Marker::X
+        } else {
+            Marker::O
+        }
 }
 
 fn is_even(index: usize) -> bool {
